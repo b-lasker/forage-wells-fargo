fb7d5901519c4371d680b9860c81d59dbe24f32a
b-lasker <bradleylasker@gmail.com>
2024-05-04 23:24:33 -0400

    implemented new classes

---
+++ src/main/java/com/wellsfargo/counselor/entity/Security.java
@@ -0,0 +1,93 @@
+package com.wellsfargo.counselor.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+@Entity
+public class Security {
+
+    @Id
+    @GeneratedValue()
+    private long securityId;
+
+    @Column(nullable = false)
+    private long portfolioId;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String category;
+
+    @Column(nullable = false)
+    private long purchasePrice;
+
+    @Column(nullable = false)
+    private String purchaseDate;
+
+    @Column(nullable = false)
+    private long quantity;
+
+    protected Security() {
+
+    }
+
+    public Security(Long portfolioId, String name, String category, Long purchasePrice,
+            String purchaseDate, Long quantity) {
+        this.portfolioId = portfolioId;
+        this.name = name;
+        this.category = category;
+        this.purchasePrice = purchasePrice;
+        this.purchaseDate = purchaseDate;
+        this.quantity = quantity;
+    }
+
+    public Long getSecurityId() {
+        return securityId;
+    }
+
+    public Long getPortfolioId() {return portfolioId; }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Long getPurchasePrice() {
+        return purchasePrice;
+    }
+
+    public void setPurchasePrice(Long purchasePrice) {
+        this.purchasePrice = purchasePrice;
+    }
+
+    public String getPurchaseDate() {
+        return purchaseDate;
+    }
+
+    public void setPurchaseDate(String purchaseDate) {
+        this.purchaseDate = purchaseDate;
+    }
+
+    public Long getQuantity() {
+        return quantity;
+    }
+
+    public void setQuantity(Long quantity) {
+        this.quantity = quantity;
+    }
+
+}
